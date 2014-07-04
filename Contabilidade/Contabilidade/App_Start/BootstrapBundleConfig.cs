00b4c5d716c9927bc2ed7e4d2de47954a2e1c851
Lucasgermani <lucas.germani@thomsonreuters.com>
2014-07-04 09:23:17 -0300

    Tentando fazer o chosen funcionar.

--- Contabilidade/Contabilidade/App_Start/BootstrapBundleConfig.cs
+++ Contabilidade/Contabilidade/App_Start/BootstrapBundleConfig.cs
@@ -14,14 +14,17 @@ namespace BootstrapSupport
                 "~/Scripts/bootstrap.js",
                 "~/Scripts/jquery.validate.js",
                 "~/scripts/jquery.validate.unobtrusive.js",
-                "~/Scripts/jquery.validate.unobtrusive-custom-for-bootstrap.js"
+                "~/Scripts/jquery.validate.unobtrusive-custom-for-bootstrap.js",
+                "~/Scripts/chosen.jquery.js"
                 ));
 
             bundles.Add(new StyleBundle("~/content/css").Include(
                 "~/Content/bootstrap.css",
                 "~/Content/body.css",
                 "~/Content/bootstrap-responsive.css",
-                "~/Content/bootstrap-mvc-validation.css"
+                "~/Content/bootstrap-mvc-validation.css",
+                "~/Content/bootstrap-mvc-validation.css",
+                "~/Content/bootstrap-chosen.css"
                 ));
         }
     }
